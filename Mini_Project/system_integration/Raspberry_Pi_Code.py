cebd41133bc73ddff700e93b3785b1b691ea9cae
Samuel Wicklund <samuel.wicklund@gmail.com>
2022-09-30 09:43:36 -0600

    Update Raspberry_Pi_Code.py

--- Mini_Project/system_integration/Raspberry_Pi_Code.py
+++ Mini_Project/system_integration/Raspberry_Pi_Code.py
@@ -43,16 +43,20 @@ def ReadfromArduino():
         print("Communication Error")
     return current_val
 
-while True:
+def SendToArduino();
     try:
-        #Send Actual Position to Arduino
         setPoint = camLoc.aruco_location(camera)
         sendPoint = setPoint.to_bytes(1, "big")
         ser.write(sendPoint)
-        usrInput = input("Start Arduino Read (Y/N): ")
-        while usrInput = 'Y':
-            currPosition = ReadfromArduino()/40;
-            lcd.message = "Set Point: " + str(setPoint) + "\nPosition: " + currPosition
+    except:
+        print("Something went Wrong")
+    return setPoint
+    
+setPoint = SendToArduino()
+while True:
+    try:
+        currPosition = (ReadfromArduino()/40) * pi;
+        lcd.message = "Set Point: " + str(setPoint) + "\nPosition: " + str(currPosition)
     except IOError:
         lcd.message = "Oops! IOError, check Pi to Arduino USB Connections"
 
