79fbb0e09375e5de357d7ce36594779d436e9ce7
Abi <amthompson@mines.edu>
2022-10-04 20:33:26 -0600

    Final Touches needs to have CV tweaked to be more responsive and accurate, and Motor Controller not 0 on initial start.

--- Mini_Project/system_integration/Raspberry_Pi_Code.py
+++ Mini_Project/system_integration/Raspberry_Pi_Code.py
@@ -16,34 +16,8 @@ import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
 import aruco_location as camLoc
 import camera_init
-import threading
 
-startTime = time.time()
-
-def DoMath(currPos, valTime):
-    sVal = str(currPos) + " " + str(valTime) + "\n"
-    print(sVal)
-    valueRet = (int(currPos)/1590 * pi)
-    return valueRet
-#Function to read serial for LCD
-def ReadfromArduino():
-    while (ser.in_waiting() < 28):
-        print("Waiting")
-    currentPosition = ser.readline().decode("utf-8").rstrip()
-    valueTime = int((time.time() - startTime) * 1000)
-    mathValue = DoMath(currentPosition, valueTime)
-    return mathValue
-        
-def SendToArduino(camera):
-    try:
-        setPoint = camLoc.aruco_location(camera)
-        sendPoint = setPoint.to_bytes(28, "big")
-        ser.write(sendPoint)
-        ser.flush()
-    except:
-        print("I couldn't send")
-    return setPoint
-
+prevSetPoint = 5; #Holds previous setpoint so Arduino doesn't get a new read command until value changes.
 
 #LCD Setup
 lcd_columns = 16
@@ -55,20 +29,49 @@ lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 lcd.clear()
 lcd.color = [0, 100, 0]
 
+
+startTime = time.time()
+
+def DoMath(currPos, valTime):
+    #sVal = str(currPos) + " " + str(valTime) + "\n" Needed for simulation values
+    #print(sVal) Needed for simulation values
+    valueRet = (int(currPos)/1590 * pi)
+    return valueRet
+
+#Function to read serial for LCD
+def ReadfromArduino():
+    currentPosition = ser.readline().decode("utf-8").rstrip()
+    valueTime = int((time.time() - startTime) * 1000)
+    mathValue = DoMath(currentPosition, valueTime)
+    return mathValue
+       
+def SendToArduino(camera):
+    #setPoint = camLoc.aruco_location(camera) Needs to e worked on
+    setPoint = 2
+    if setPoint != prevSetPoint:
+        ser.write(str(setPoint).encode("utf-8"))
+        ser.flush()
+        ser.reset_input_buffer()
+        return setPoint
+    else:
+        return prevSetPoint
+
+
 #Set address
-ser = serial.Serial('/dev/ttyACM0', 115200)
-#Wait for connection to complete
-time.sleep(5)
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
+ser.reset_output_buffer()
+ser.flush()
+time.sleep(2)
 
 #Setup Camera
 camera = camera_init.camera_init()
-setPoint = SendToArduino(camera)
-print("Sleeping 2 seconds to give Arduino time to send something back")
-time.sleep(2)
-print("Setpoint is: ", setPoint)
+
 while True:
     try:
-        currPosition = ReadfromArduino()
-        #lcd.message = "Set Point: " + str(setPoint) + "\nPosition: " + str(currPosition)
+        prevSetPoint = SendToArduino(camera) #Each loop detect and send new Setpoint
+        while ser.in_waiting < 2:
+            pass
+        currePosition = ReadfromArduino()
+        lcd.message = "Set Point: " + str(prevSetPoint) + "\nPosition: " + str(currePosition)
     except IOError:
         lcd.message = "Oops! IOError, check Pi to Arduino USB Connections"
