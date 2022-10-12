c33bc340f34840da3914b98ef0b0f18cb2c07948
etellschow <etellschow@mines.edu>
2022-10-12 11:45:46 -0600

    arduino code

--- Demo_1/Demo1Loc/test2.cpp
+++ Demo_1/Demo1Loc/test2.cpp
@@ -1,5 +1,2 @@
-#include <Encoder.h>
+#include "Encoder.h"
 #include "test2.h"
-
-Encoder leftWheel(LEFT_PIN_A, LEFT_PIN_B);
-Encoder rightWheel(RIGHT_PIN_A, RIGHT_PIN_B);
--- Demo_1/Demo1Loc/test2.h
+++ Demo_1/Demo1Loc/test2.h
@@ -1,5 +1,1 @@
 // Move forward in a straight line and stop after a specified distance in feet (between 1 and 10 feet).
-#define LEFT_PIN_A  2
-#define RIGHT_PIN_A 3
-#define LEFT_PIN_B  4
-#define RIGHT_PIN_B 5
