f9ff1146090713c9691a16095795e87fe0a2d3e5
etellschow <etellschow@mines.edu>
2022-10-14 10:55:55 -0600

    Renamed Demo1Loc to arduino

---
+++ Demo_1/Arduino/test2.cpp
@@ -0,0 +1,9 @@
+// Move forward in a straight line and stop after a specified distance in feet (between 1 and 10 feet).
+
+#include "Encoder.h"
+#include "test2.h"
+
+int inchesToCounts(int inches) {
+  int counts = 0;
+  return counts;
+}
