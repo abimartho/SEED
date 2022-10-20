e8ca5e2efa80e928447245cdd0bfc5eeca73f0c6
Samuel Wicklund <samuel.wicklund@gmail.com>
2022-10-20 04:22:24 -0600

    Task 1 Updated

--- Demo_1/Pi/Demo1_Task1_Pi.py
+++ Demo_1/Pi/Demo1_Task1_Pi.py
@@ -1,1 +1,12 @@
 #This File is for Demo 1 Code on the Pi
+import I2CLCD as LCD
+#import aruco_location as compVis
+#import camera_init as camInst
+
+LCD.initializeLCD()
+#Aruco Marker Code
+#camera = camInst.camera_init()
+#foo = compVis.aruco_location(camera)
+#writeLCD(foo)
+#Delay for 60-120s then
+#lcdOff()
