dbdbbb5ee0919ece72983699262272f3984e8e3f
Samuel Wicklund <samuel.wicklund@gmail.com>
2022-10-22 09:10:24 -0600

    Potential fix to I2CLCD
    - Code changes to I2CLCD & Demo1_Task1_Pi.
    - If code still doesn't cause message to display on LCD, then ensure that within options on the PI I2C is enabled. If it is not this needs to be enabled.

--- Demo_1/Pi/Demo1_Task1_Pi.py
+++ Demo_1/Pi/Demo1_Task1_Pi.py
@@ -4,13 +4,17 @@ import aruco_location as compVis
 import cv_helpers as camInst
 import time
 
-LCD.initializeLCD()
+lcd = LCD.initializeLCD()
 #Aruco Marker Code
 camera = camInst.camera_init()
+
+#Size of Aruco Marker in (cm)
+dimension = 5
+
 #Loop below angle call with slight delay
 while True:
-    angle = compVis.aruco_location(camera)
-    writeLCD(angle)
+    angle = compVis.aruco_location(camera, dimension)
+    writeLCD(str(angle))
     delay(2)
 
 #lcdOff()
